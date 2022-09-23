8419fd9341f4fe71b92e5d5deb4cb07437a127ba
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2022-09-23 15:27:24 -0700

    Create OxBS.R

---
+++ OxBS.R
@@ -0,0 +1,14 @@
+#OxBS Array Processing
+#STEP1. Load necessary R packages for analysis
+library(sesame)
+library(colorRamps)
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(gplots)
+library(grDevices)
+library(reshape2)
+library(tidyverse)
+library(minfi)
+library(RColorBrewer)
+library(ENmix)
